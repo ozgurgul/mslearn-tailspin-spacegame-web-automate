d2e67f7b1e5728a37523777ab926e24c277b52f1
Ozgur Gul <Ozgur.Gul@hpe.com>
2021-04-23 12:05:01 +0100

    main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,9 @@
 # We strongly recommend using the required_providers block to set the
 # Azure Provider source and version being used
 terraform {
+  
+   backend "azurerm" {
+  }
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
