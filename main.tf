30ca5bd03387476d5734c40a96169fdf534d1381
Ozgur Gul <Ozgur.Gul@hpe.com>
2021-04-24 23:16:24 +0100

    changed to azurerm_linux_virtual_machine

--- main.tf
+++ main.tf
@@ -214,9 +214,9 @@ resource "azurerm_managed_disk" "data_disk" {
   disk_size_gb         = 10
 }
 
-resource "azurerm_virtual_machine_data_disk_attachment" "example" {
-  managed_disk_id    = azurerm_managed_disk.data_disk.id
-  virtual_machine_id = azurerm_linux_virtual_machine.vm.id
+resource "azurerm_virtual_machine_data_disk_attachment" "data-disk" {
+  managed_disk_id    = azurerm_managed_disk.data_disk[count.index].id
+  virtual_machine_id = azurerm_linux_virtual_machine.vm[count.index].id
   lun                = "10"
   caching            = "ReadWrite"
 }
