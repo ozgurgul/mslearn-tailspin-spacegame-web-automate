7497827a07acec7a67986646cb7be4da267e7b96
Ozgur Gul <Ozgur.Gul@hpe.com>
2021-04-24 22:55:10 +0100

    changed to azurerm_linux_virtual_machine

--- main.tf
+++ main.tf
@@ -141,80 +141,8 @@ resource "azurerm_network_interface" "nic" {
   tags = azurerm_resource_group.rg.tags
 }
 
-
-resource "azurerm_virtual_machine" "vm" {
-  count                 = var.vm_count
-  name                  = format("vm-%02d", count.index + 1)
-  location              = azurerm_resource_group.rg.location
-  resource_group_name   = azurerm_resource_group.rg.name
-  network_interface_ids = [element(azurerm_network_interface.nic.*.id, count.index)]
-  vm_size               = var.vm_size
-
-  # Uncomment this line to delete the OS disk automatically when deleting the VM
-  # delete_os_disk_on_termination = true
-
-  # Uncomment this line to delete the data disks automatically when deleting the VM
-  # delete_data_disks_on_termination = true
-
-  storage_image_reference {
-    publisher = element(split("/", var.vm_image_string), 0)
-    offer     = element(split("/", var.vm_image_string), 1)
-    sku       = element(split("/", var.vm_image_string), 2)
-    version   = element(split("/", var.vm_image_string), 3)
-  }
-  
-  plan {
-    publisher = element(split("/", var.vm_image_string), 0)
-    name      = element(split("/", var.vm_image_string), 1)
-    product   = element(split("/", var.vm_image_string), 2)
-  }
-
-  storage_os_disk {
-    name                = format("vm-%02d-OS-Disk", count.index + 1)
-    caching             = "ReadWrite"
-    managed_disk_type   = "Standard_LRS"
-    create_option       = "FromImage"
-  }
-
-  # Optional data disks
-  storage_data_disk {
-    name                = format("vm-%02d-Data-Disk", count.index + 1)
-    disk_size_gb        = "20"
-    managed_disk_type   = "Standard_LRS"
-    create_option       = "Empty"
-    lun                 = 0
-  }
-
-  os_profile {
-    computer_name  = format("host-%02d", count.index + 1)
-    admin_username = var.admin_username
-    admin_password = var.admin_password
-  }
-
-  # dynamic "os_profile_linux_config" {
-  #   for_each = var.vm_type == "Linux" ? [1] : []
-  #   content {
-  #     disable_password_authentication = var.os_profile_linux_config_disable_password_authentication
-
-  #     dynamic "ssh_keys" {
-  #       for_each = var.os_profile_linux_config_ssh_keys
-
-  #       content {
-  #         key_data = ssh_keys.value.key_data
-  #         path     = format("/home/%s/.ssh/authorized_keys", var.os_profile_admin_username)
-  #       }
-  #     }
-  #   }
-  # }
-
-  os_profile_linux_config {
-    disable_password_authentication = false
-  }
-
-  tags = azurerm_resource_group.rg.tags
-}
-
 ###########################################################
+
 # Create (and display) an SSH key
 resource "tls_private_key" "example_ssh" {
   algorithm = "RSA"
@@ -230,15 +158,15 @@ resource "azurerm_linux_virtual_machine" "vm" {
   location              = azurerm_resource_group.rg.location
   resource_group_name   = azurerm_resource_group.rg.name
   network_interface_ids = [element(azurerm_network_interface.nic.*.id, count.index)]
-  vm_size               = var.vm_size
+  size               = var.vm_size
 
   os_disk {
     name                = format("vm-%02d-OS-Disk", count.index + 1)
     caching             = "ReadWrite"
-    managed_disk_type   = "Standard_LRS"
+    storage_account_type   = "Standard_LRS"
   }
 
-  storage_image_reference {
+  source_image_reference  {
     publisher = element(split("/", var.vm_image_string), 0)
     offer     = element(split("/", var.vm_image_string), 1)
     sku       = element(split("/", var.vm_image_string), 2)
@@ -272,6 +200,7 @@ resource "azurerm_linux_virtual_machine" "vm" {
 
 # Optional data disks
 resource "azurerm_managed_disk" "data_disk" {
+  count                = var.vm_count
   name                 = format("vm-%02d-OS-Disk", count.index + 1)
   location             = azurerm_resource_group.rg.location
   resource_group_name  = azurerm_resource_group.rg.name
@@ -281,8 +210,8 @@ resource "azurerm_managed_disk" "data_disk" {
 }
 
 resource "azurerm_virtual_machine_data_disk_attachment" "example" {
-  managed_disk_id    = azurerm_managed_disk.data_disk.id
-  virtual_machine_id = azurerm_linux_virtual_machine.vm.id
+  managed_disk_id    = azurerm_managed_disk.data_disk.*.id
+  virtual_machine_id = azurerm_linux_virtual_machine.vm.*.id
   lun                = "10"
   caching            = "ReadWrite"
 }
