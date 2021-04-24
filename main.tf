6a087ed05ab90367545ac2e25093d066605cfd5e
Ozgur Gul <Ozgur.Gul@hpe.com>
2021-04-24 23:03:22 +0100

    changed to azurerm_linux_virtual_machine

--- main.tf
+++ main.tf
@@ -148,7 +148,11 @@ resource "tls_private_key" "example_ssh" {
   algorithm = "RSA"
   rsa_bits = 4096
 }
-output "tls_private_key" { value = tls_private_key.example_ssh.private_key_pem }
+output "tls_private_key" { 
+  value = tls_private_key.example_ssh.private_key_pem 
+  # sensitive must be true when referencing a sensitive input variable
+  sensitive = true
+}
 
 # Create virtual machine
 resource "azurerm_linux_virtual_machine" "vm" {
